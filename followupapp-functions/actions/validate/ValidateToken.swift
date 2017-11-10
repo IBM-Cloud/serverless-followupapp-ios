67714086a5aaa0272b645327b4c8988691c89eb4
Vidyasagar Machupalli <vidyasagar.msc@in.ibm.com>
2017-11-10 18:24:16 +0530

    placeholders actions added

---
+++ followupapp-functions/actions/validate/ValidateToken.swift
@@ -0,0 +1,8 @@
+/*Placeholder code*/
+func main(args: [String:Any]) -> [String:Any] {
+    if let name = args["name"] as? String {
+        return [ "greeting" : "Hello \(name)!" ]
+    } else {
+        return [ "greeting" : "Hello stranger!" ]
+    }
+}
