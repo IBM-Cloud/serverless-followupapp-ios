0f9f1ddde4feeeab9b0a4bb49dd6758d8a15041b
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-06-04 11:30:10 +0200

    update to new push notifications action

--- actions/users/PrepareUserNotification.swift
+++ actions/users/PrepareUserNotification.swift
@@ -8,7 +8,7 @@ import Foundation
 
 func main(args: [String:Any]) -> [String:Any] {
     print("Received:",args)
-    
+
     var str = ""
     var responseValue = ""
     var name = ""
@@ -71,8 +71,8 @@ func main(args: [String:Any]) -> [String:Any] {
         }
     }
     responseData = [
-        "deviceIds": [deviceIds],
-        "text": notificationText
+        "targetDeviceIds": [deviceIds],
+        "messageText": notificationText
     ]
     
     return responseData
